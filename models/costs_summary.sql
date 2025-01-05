ec042fab578c61600cb8d485437dd57443dea01c
Pavel Roslovets <p.v.roslovets@gmail.com>
2025-01-05 16:54:22 +0100

    feat: remove costs_summary model

--- models/costs_summary.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-    "month",
-    sum(cost_rub) as cost_rub
-FROM {{ ref('costs_breakdown')}}
-GROUP BY ALL
--- models/profit.sql
+++ models/profit.sql
@@ -1,3 +1,10 @@
+WITH costs_summary_cte AS (
+    SELECT
+        "month",
+        sum(cost_rub) as cost_rub
+    FROM {{ ref('costs_breakdown')}}
+    GROUP BY ALL
+)
 SELECT
     "month",
     cost_rub::int as cost_rub,
@@ -5,7 +12,7 @@ SELECT
     (revenue_rub - cost_rub)::int as profit_rub,
     (profit_rub / users_count)::int as profit_per_user_rub,
     (profit_rub / trips_count)::int as profit_per_trip_rub
-FROM {{ ref('costs_summary')}}
+FROM costs_summary_cte
 JOIN {{ ref('trips_monthly')}}
     USING ("month")
 ORDER BY 1
