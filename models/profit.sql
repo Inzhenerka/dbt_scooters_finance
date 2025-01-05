d1a46bff2452d2d7bff9fe66a4817476f11dd269
Pavel Roslovets <p.v.roslovets@gmail.com>
2025-01-05 16:02:51 +0100

    feat: descriptions and tests

--- models/profit.sql
+++ models/profit.sql
@@ -1,7 +1,7 @@
 SELECT
     "month",
-    cost_rub as cost_rub,
-    revenue_rub as revenue_rub,
+    cost_rub,
+    revenue_rub,
     revenue_rub - cost_rub as profit_rub
 FROM {{ ref('total_costs')}}
 JOIN {{ ref('trips_monthly')}}
